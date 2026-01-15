dbaa50f6326415217aaab179dfc749a7475f9943
francesco <massafra32@gmail.com>
2026-01-15 13:08:23 +0100

    feat: Increase numerical library thread limits to 4 and add environment variable logging.

--- src/utils/env_setup.py
+++ src/utils/env_setup.py
@@ -2,8 +2,13 @@ import os
 
 # Set environment variables to limit threads before any other imports
 # This is critical to prevent segmentation faults with OpenBLAS/Harmony
-os.environ['OPENBLAS_NUM_THREADS'] = '1'
-os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['MKL_NUM_THREADS'] = '1'
-os.environ['VECLIB_MAXIMUM_THREADS'] = '1'
-os.environ['NUMEXPR_NUM_THREADS'] = '1'
+os.environ['OPENBLAS_NUM_THREADS'] = '4'
+os.environ['OMP_NUM_THREADS'] = '4'
+os.environ['MKL_NUM_THREADS'] = '4'
+os.environ['VECLIB_MAXIMUM_THREADS'] = '4'
+os.environ['NUMEXPR_NUM_THREADS'] = '4'
+
+print("--- Environment Setup ---")
+for var in ['OPENBLAS_NUM_THREADS', 'OMP_NUM_THREADS', 'MKL_NUM_THREADS', 'VECLIB_MAXIMUM_THREADS', 'NUMEXPR_NUM_THREADS']:
+    print(f"{var}: {os.environ.get(var)}")
+print("-------------------------")
