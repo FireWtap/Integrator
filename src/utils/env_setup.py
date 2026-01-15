88bd314ef0847b91c0e39f7d9399b55bae6fd925
francesco <massafra32@gmail.com>
2026-01-15 15:09:26 +0100

    environ

--- src/utils/env_setup.py
+++ src/utils/env_setup.py
@@ -2,11 +2,11 @@ import os
 
 # Set environment variables to limit threads before any other imports
 # This is critical to prevent segmentation faults with OpenBLAS/Harmony
-os.environ['OPENBLAS_NUM_THREADS'] = '4'
-os.environ['OMP_NUM_THREADS'] = '4'
-os.environ['MKL_NUM_THREADS'] = '4'
-os.environ['VECLIB_MAXIMUM_THREADS'] = '4'
-os.environ['NUMEXPR_NUM_THREADS'] = '4'
+os.environ["OPENBLAS_NUM_THREADS"] = "32"
+os.environ["OMP_NUM_THREADS"] = "32"
+os.environ["MKL_NUM_THREADS"] = "32"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "32"
+os.environ["NUMEXPR_NUM_THREADS"] = "32"
 
 print("--- Environment Setup ---")
 for var in ['OPENBLAS_NUM_THREADS', 'OMP_NUM_THREADS', 'MKL_NUM_THREADS', 'VECLIB_MAXIMUM_THREADS', 'NUMEXPR_NUM_THREADS']:
